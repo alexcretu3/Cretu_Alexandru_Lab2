cf16832f418c3883f2a3ed4d9744527d48b047c7
Alex <Alex@DESKTOP-RNI202U>
2022-11-03 19:37:28 +0200

    Aggiungere i file di progetto.

---
+++ Data/Cretu_Alexandru_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Cretu_Alexandru_Lab2.Models;
+
+namespace Cretu_Alexandru_Lab2.Data
+{
+    public class Cretu_Alexandru_Lab2Context : DbContext
+    {
+        public Cretu_Alexandru_Lab2Context (DbContextOptions<Cretu_Alexandru_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cretu_Alexandru_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Cretu_Alexandru_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Cretu_Alexandru_Lab2.Models.Category> Category { get; set; }
+    }
+}
