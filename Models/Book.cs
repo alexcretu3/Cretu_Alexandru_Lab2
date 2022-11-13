8bd05ba21fe9c6dfda0484f8d5bd7758afaafa56
Bianca Cretu <bianca.cretu@analog.com>
2022-11-13 13:19:58 +0200

    lab3

--- Models/Book.cs
+++ Models/Book.cs
@@ -7,10 +7,13 @@ namespace Cretu_Alexandru_Lab2.Models
 {
     public class Book
     {
+        [Required, StringLength(150, MinimumLength = 3)]
         public int ID { get; set; }
         [Display(Name = "Book Title")]
         public string Title { get; set; }
+        [RegularExpression(@"^[A-Z][a-z]+\s[A-Z][a-z]+$", ErrorMessage = "Numele autorului trebuie sa fie de forma 'Prenume Nume'"), Required, StringLength(50, MinimumLength = 3)]
         public string Author { get; set; }
+        [Range(1, 300)]
         [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
